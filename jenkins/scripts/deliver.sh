fb9a268e68b09e9bd73310fc29a907e26c7ea209
Samia El Amarti <samia.elamarti11@gmail.com>
2025-07-11 22:41:03 +0100

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,26 +1,16 @@
 #!/usr/bin/env bash
 
-echo 'The following Maven command installs your Maven-built Java application'
-echo 'into the local Maven repository, which will ultimately be stored in'
-echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
-echo 'volume).'
+echo 'Installing the JAR to local Maven repository...'
 set -x
-mvn jar:jar install:install help:evaluate -Dexpression=project.name
+/opt/maven/bin/mvn install -DskipTests
 set +x
 
-echo 'The following command extracts the value of the <name/> element'
-echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
+echo 'Extracting project name and version from pom.xml...'
 set -x
-NAME=`mvn -q -DforceStdout help:evaluate -Dexpression=project.name`
+NAME=$(/opt/maven/bin/mvn -q -DforceStdout help:evaluate -Dexpression=project.name)
+VERSION=$(/opt/maven/bin/mvn -q -DforceStdout help:evaluate -Dexpression=project.version)
 set +x
 
-echo 'The following command behaves similarly to the previous one but'
-echo 'extracts the value of the <version/> element within <project/> instead.'
-set -x
-VERSION=`mvn -q -DforceStdout help:evaluate -Dexpression=project.version`
-set +x
-
-echo 'The following command runs and outputs the execution of your Java'
-echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
+echo "Running built JAR: target/${NAME}-${VERSION}.jar"
 set -x
 java -jar target/${NAME}-${VERSION}.jar
